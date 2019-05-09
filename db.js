2ff86fab9889127fa5666885785a31212a5bb413
spookyTanya <tanyabilanyuk@gmail.com>
2019-05-09 21:11:33 +0300

    db

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 
 var con = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "project"
+  host: "us-cdbr-iron-east-02.cleardb.net",
+  user: "bad8119fff86d4",
+  password: "0a0ab0a6",
+  database: "heroku_d2d4fb60e781177"
 });
 
 
