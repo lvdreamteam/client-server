9507680c0d066025f1001254d0e2488bfecef8cd
spookyTanya <tanyabilanyuk@gmail.com>
2019-05-09 19:59:11 +0300

    initial

--- app.js
+++ app.js
@@ -3,12 +3,8 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const session = require('express-session');
-var MySQLStore = require('express-mysql-session')(session);
 const bodyParser = require('body-parser');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 const mainRouter = require('./routes/main');
 
 const app = express();
@@ -22,48 +18,24 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static("public"));
-
+app.use(express.static(path.join(__dirname, 'public')));
+// app.use('/images', express.static(path.join(__dirname + '/images')));
+// app.use(express.static('public/images'));
 
 app.use(bodyParser.urlencoded({
 	extended: true
 }));
 
-let options = {
-	host: "us-cdbr-iron-east-03.cleardb.net",
-  	user: "b137b481565ba4",
-  	password: "3d0e4252",
-  	database: "heroku_656924a3d6f9fc3"
-};
-var sessionStore = new MySQLStore(options);
+// let options = {
+// 	host: "us-cdbr-iron-east-03.cleardb.net",
+//   	user: "b137b481565ba4",
+//   	password: "3d0e4252",
+//   	database: "heroku_656924a3d6f9fc3"
+// };
+// var sessionStore = new MySQLStore(options);
 
-app.use(session({
-	key: 'myCookie',
-	secret: 'secret12345',
-	store: sessionStore,
-	resave: false,
-	saveUninitialized: false,
-	cookie: {
-		originalMaxAge: 1000 * 60 * 60,
-		maxAge: 1000 * 60 * 60,
-		//secure: true,
-		path: '/',
-		//httpOnly: false,
-	}
-}));
-
-app.use((req, res, next) => {
-	req.session.init = 'init';
-	next();
-});
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/main', mainRouter);
-app.get('/', function(req, res){
-	res.redirect('/main');
-});
+
 app.get('/favicon.ico', (req, res) => res.sendStatus(204));
 
 // catch 404 and forward to error handler
--- db.js
+++ db.js
@@ -1,18 +1,11 @@
 var mysql = require('mysql');
 
 var con = mysql.createPool({
-  host: "us-cdbr-iron-east-03.cleardb.net",
-  user: "b137b481565ba4",
-  password: "3d0e4252",
-  database: "heroku_656924a3d6f9fc3"
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "project"
 });
 
-/*con.connect(function(err) {
-  console.log("Connected!");
-});*/
-
-/*setInterval(function () {
-    con.query('SELECT 1 from users');
-}, 5000);*/
 
 module.exports = con;
