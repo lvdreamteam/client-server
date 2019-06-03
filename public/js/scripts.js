fdd885535d87717a6dab9d02095440c7e7367e1a
StepanViliura <vilyura.stepan@gmail.com>
2019-06-03 04:21:00 +0300

    admin
    done

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -7,7 +7,7 @@ const logout = function (e) {
         let response = JSON.parse(request.response);
         window.location.href = response.redirectUrl;
      });
-    request.send();
+    request.send(JSON.stringify({reques:"lol"}));
 }
 
 const login = function (e, email, password) {
@@ -28,4 +28,89 @@ const login = function (e, email, password) {
         }
      });
     request.send(JSON.stringify({email: email, password:password}));
+}
+
+const changePassword = function (e, oldPassword, newPassword) {
+    e.preventDefault();
+    let request = new XMLHttpRequest();
+    request.open("POST", "/admin/changePassword", true);   
+    request.setRequestHeader("Content-Type", "application/json");
+    request.withCredentials = false;
+    request.addEventListener("load", function () {
+        console.log(request.response);
+        const response = JSON.parse(request.response);
+        if (response.error) {
+            alert(response.error);
+        }
+        if(response.success){
+            alert(response.success);
+        }
+    });
+    console.log(oldPassword);
+    console.log(newPassword);
+    request.send(JSON.stringify({oldPassword: oldPassword, newPassword:newPassword}));
+}
+
+const createNewAdmin = function (e, username, password) {
+    e.preventDefault();
+    let request = new XMLHttpRequest();
+    request.open("POST", "/admin/addNewAdmin", true);   
+    request.setRequestHeader("Content-Type", "application/json");
+    request.withCredentials = false;
+    request.addEventListener("load", function () {
+        console.log(request.response);
+        const response = JSON.parse(request.response);
+        if (response.error) {
+            alert(response.error);
+        }
+        if(response.success){
+            alert(response.success);
+        }
+    });
+    console.log(username);
+    console.log(password);
+    request.send(JSON.stringify({username: username, password:password}));
+}
+
+const addSympthoms= function (e, sympthom) {
+    e.preventDefault();
+    let request = new XMLHttpRequest();
+    request.open("POST", "/admin/addSympthoms", true);   
+    request.setRequestHeader("Content-Type", "application/json");
+    request.withCredentials = false;
+    request.addEventListener("load", function () {
+        console.log(request.response);
+        const response = JSON.parse(request.response);
+        if (response.error) {
+            alert(response.error);
+        }
+        if(response.success){
+            alert(response.success);
+        }
+    });
+    request.send(JSON.stringify({sympthom: sympthom}));
+}
+
+const addPreparat = function (e, name, description,  sympthoms, protypokazania) {
+    e.preventDefault();
+    let request = new XMLHttpRequest();
+    request.open("POST", "/admin/addPreparat", true);   
+    request.setRequestHeader("Content-Type", "application/json");
+    request.withCredentials = false;
+    request.addEventListener("load", function () {
+        console.log(request.response);
+        const response = JSON.parse(request.response);
+        if (response.error) {
+            alert(response.error);
+        }
+        if(response.success){
+            alert(response.success);
+        }
+    });
+    request.send(JSON.stringify({
+        name: name,
+        description: description,
+        sympthoms: sympthoms,
+        protypokazania: protypokazania
+    }));
 }
--- routes/admin.js
+++ routes/admin.js
@@ -27,7 +27,21 @@ const saveAdminIdToCookie = function (res, id) {
 adminRouter.get('/', function(req, res) {
     if (req.cookies.adminId && req.cookies.adminId != 'undefined') {
         console.log(req.cookies);
-        res.render('settings');
+        const sql = 'SELECT * FROM `parametr`';
+        let symptomsArr = [];
+        db.query(sql, (error, result, fields) => {
+            if(error){
+                console.log(error);
+            }
+            for(let i=0; i<result.length; i++) {
+                let data = {
+                    id: result[i].id,
+                    name: result[i].name
+                };
+                symptomsArr.push(data);
+            }
+            res.render('settings', {symptoms: symptomsArr});
+        });
     }
     else {
         console.log("OY MAY");
@@ -35,6 +49,8 @@ adminRouter.get('/', function(req, res) {
         res.redirect('/admin/login');
     }
 });
+
+
 adminRouter.post('/logout', function(req, res) {
     saveAdminIdToCookie(res, undefined);
     res.json({
@@ -43,6 +59,120 @@ adminRouter.post('/logout', function(req, res) {
             host: req.get('host'),
             pathname: '/admin'
         })
+    });
+});
+
+adminRouter.post('/changePassword', function(req, res) {
+    console.log(req.body);
+    if (req.cookies.adminId && req.cookies.adminId != 'undefined') {
+        const main = () => {
+            res.json({
+                redirectUrl: url.format({
+                    protocol: req.protocol,
+                    host: req.get('host'),
+                    pathname: '/main'
+                })
+            });
+        }
+        const hash = crypto.createHash('sha256');
+        console.log("adminId: " + req.cookies.adminId);      
+        const sql = 'SELECT * FROM admins WHERE id = "' + req.cookies.adminId + '" AND password = "' + hash.update(req.body.oldPassword).digest('base64') +'"';
+        db.query(sql, (error, result, fields) => {
+            if (error) {
+                main();
+            }
+            if (result && result[0]) {
+                console.log("newPassword " + req.body.newPassword);
+                const hash2 = crypto.createHash('sha256');
+                const update = 'UPDATE admins SET password = "' + hash2.update(req.body.newPassword).digest('base64') + '" WHERE id = "' + + req.cookies.adminId + '"';
+                db.query(update, (error, result, fields) => {
+                    if (error) {
+                        main();
+                    }
+                    console.log("result = " + result);
+                    if(result){
+                        console.log("oldPassword " + req.body.oldPassword);
+                        res.json({success: "Пароль змінено!"});
+                    }
+                });
+            }
+            else {
+                console.log("result " + result[0]);
+                res.json({error: "Невірнй пароль"});
+            }
+        });
+    }
+});
+
+adminRouter.post('/addNewAdmin', function(req, res) {
+    const hash = crypto.createHash('sha256');
+    console.log(req.body);
+    var sql = "INSERT INTO admins (email, password) VALUES ('"+ req.body.username +"', '"+ hash.update(req.body.password).digest('base64') +"')";
+    db.query(sql, (error, result, fields) => {
+        if (error) {
+            console.log("error");
+            res.json({error: "Щось пішло не так :("});
+        }
+        if (result) {
+            console.log(result);
+            res.json({success: "Адміністратора додано!"});
+        }
+        else {
+            console.log(result);
+            res.json({error: "Щось пішло не так :("});
+        }
+    });
+});
+
+adminRouter.post('/addPreparat', function(req, res) {
+    var sql = "INSERT INTO preparat (name, description) VALUES ('"+ req.body.name +"', '"+ req.body.description +"')";
+    db.query(sql, (error, result, fields) => {
+        if (error) {
+            console.log("error");
+            res.json({error: "Щось пішло не так :("});
+        }
+        if (result) {
+            for(let i of req.body.sympthoms) {
+                var pokazannia = "INSERT INTO pokazannia (preparat_id, parametr_id) VALUES ('"+ result.insertId +"', '"+ i +"')";
+                db.query(pokazannia, (error, result, fields) => {
+                    if (error) {
+                        console.log("sympthoms error");
+                    }
+                });
+            }
+            for(let i of req.body.protypokazania) {
+                var pokazannia = "INSERT INTO protypokazannia (preparat_id, parametr_id) VALUES ('"+ result.insertId +"', '"+ i +"')";
+                db.query(pokazannia, (error, result, fields) => {
+                    if (error) {
+                        console.log("protypokazania error");
+                    }
+                });
+            }                           
+            res.json({success: req.body});
+        }
+        else {
+            console.log(result);
+            res.json({error: "Щось пішло не так :("});
+        }
+    });
+});
+
+adminRouter.post('/addSympthoms', function(req, res) {
+    console.log(req.body);
+    var sql = "INSERT INTO parametr (name) VALUES ('"+ req.body.sympthom +"')";
+    db.query(sql, (error, result, fields) => {
+        if (error) {
+            console.log("error");
+            res.json({error: "Щось пішло не так :("});
+        }
+        if (result) {
+            console.log(result);
+            res.json({success: "Симптом додано!"});
+        }
+        else {
+            console.log(result);
+            res.json({error: "Щось пішло не так :("});
+        }
     });
 });
 
